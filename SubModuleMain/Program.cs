fb46966b7de4e8fd81adfec25e7ef0d6193be966
taru-nayak <tarangnayak@gmail.com>
2023-04-04 18:09:13 +0530

    updated

--- SubModuleMain/Program.cs
+++ SubModuleMain/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using CommonLIB;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Test submodule!");
+Console.WriteLine(CommonConstant.ProjectName);
+Console.ReadLine();
